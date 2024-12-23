5cf53cbbd5eccec3f40f0fcf8915fabdd0b3d1c1
Raheel Ahmed <raheelahmed017@gmail.com>
2024-12-23 20:19:16 +0500

    Changes

--- app/Http/Resources/Api/V1/ApplicationResource.php
+++ app/Http/Resources/Api/V1/ApplicationResource.php
@@ -3,7 +3,6 @@
 namespace App\Http\Resources\Api\V1;
 
 use App\Http\Resources\Api\DateResource;
-use App\Models\ApplicationProcess;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
